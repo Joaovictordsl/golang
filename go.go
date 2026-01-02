cafefabb58abb49c8d4c762d852a85401c30814a
Joao Victor de Sousa Lima <joaovictordesousalima@192.168.0.8>
2026-01-01 23:59:15 -0300

    go leet

--- go.go
+++ go.go
@@ -1,13 +1,29 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"GO/newdir"
-)
+//pega o resto da divisao por 10 e armazena numa variavel
+//pega o inteiro que tem e divide por 10, e multi por 10
+//pega o valor multiplicado e soma com o resto
+
+// func isPalindrome(x int) bool{
+
+// 	x
+
+// 	return
+// }
 
 func main() {
+	original := 323
+	ultimo_digito := 0
+	invertido := 0
 
-	fmt.Println("teste")
-	fmt.Println(newdir.Sum(3))
+	ultimo_digito = original % 10
+	invertido = (invertido * 10) + ultimo_digito
+	original = original / 10
+
+	fmt.Println(ultimo_digito)
+	fmt.Println(invertido)
+	fmt.Println(original)
+
 }
