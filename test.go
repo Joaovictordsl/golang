a16bcc07a7ce540d88501b148c9bbd76615a66f5
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-02 23:12:11 -0300

    prefix

--- test.go
+++ test.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	romanToInt("MMXXIVV")
-	// fmt.Println(-1 + 5)
+	str := []string{"Ola", "teste", "eu"}
+	result := []string{}
 
+	encontrados := make(map[string]bool)
+
+	// fmt.Printf("%c\n", str[0][1])
+
+	// result = append(result, string(str[0][0]))
+
+	for i := 0; i < len(str); i++ {
+		primeiraLetra := string(str[i][0])
+
+		if !encontrados[primeiraLetra] {
+			result = append(result, primeiraLetra)
+			encontrados[primeiraLetra] = true
+		}
+
+	}
+
+	fmt.Println(result)
 }
