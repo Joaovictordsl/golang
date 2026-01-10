a09bf152a115583e65ca0697781d3e69bc7539bb
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-10 17:28:50 -0300

    gogo

--- algorithms/README.md
+++ algorithms/README.md
@@ -3,4 +3,3 @@
 The binary search is an algorithm that divide the elements of an array on half, verifies if the number that you want to find is lesser or greater than the position the mid part is at right now. Then, if its lesser, all the positions lesser than the middle will be discarded, as the lowest position or first position is now the middle one. And vice versa if its greater.
 
 
-
---
+++ go.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+
+func binary(array []int, num int) int{
+
+low := 0
+high := len(array) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := array[mid]
+
+		if guess == num {
+			return mid 
+		
+		} else if num < guess {
+			high = mid - 1
+		} else {
+			low = mid + 1		
+		}
+
+	}
+
+return -1
+}
+
+
+func main(){
+	x := []int{1,3, 10, 11, 34, 45,78, 100, 220, 345, 555}
+
+	fmt.Println(binary(x, 345))
+
+}
+
