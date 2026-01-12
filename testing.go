8b32e7080c7090d0af42b7f4815c08ae891e7428
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-01-11 23:59:09 -0300

    update3

--- testing.go
+++ testing.go
@@ -3,4 +3,14 @@ package main
 func binary(array []int, target int) int {
 	low := 0
 	high := len(array) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := array[mid]
+
+		if guess == target {
+			return mid
+		}
+
+	}
 }
